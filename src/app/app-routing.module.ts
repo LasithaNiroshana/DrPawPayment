25b0b7bbafb43af7714c26a6b72be39f879639f7
LasithaNiroshana <lasithaniroshanawm@gmail.com>
2022-11-24 10:13:08 +0530

    Added webpay gateway

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { OtpPageComponent } from './Pages/otp-page/otp-page.component';
 import { ConfirmOtpComponent } from './Pages/confirm-otp/confirm-otp.component';
 import { PaymentConfirmPageComponent } from './Pages/payment-confirm-page/payment-confirm-page.component';
+import { CustomerDetailsPageComponent } from './Pages/customer-details-page/customer-details-page.component';
+
 
 const routes: Routes = [
   {component:OtpPageComponent,path:''},
   {component:OtpPageComponent,path:'otp'},
   {component:ConfirmOtpComponent,path:'confirmOtp'},
-  {component:PaymentConfirmPageComponent,path:'paymentConfirm'}
+  {component:PaymentConfirmPageComponent,path:'paymentConfirm'},
+  {component:CustomerDetailsPageComponent,path:'customerDetails'}
 ];
 
 @NgModule({
@@ -19,5 +22,6 @@ export class AppRoutingModule { }
 export const routingComponents=[
   OtpPageComponent,
   ConfirmOtpComponent,
-  PaymentConfirmPageComponent
+  PaymentConfirmPageComponent,
+  CustomerDetailsPageComponent
 ];
